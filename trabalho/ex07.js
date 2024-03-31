8789c0c46d099e6c32ec34bb05c2a5debd1c9624
DimitriPorto03 <133895461+DimitriPorto03@users.noreply.github.com>
2024-03-31 14:41:40 -0300

    Update ex07.js

--- trabalho/ex07.js
+++ trabalho/ex07.js
@@ -1,1 +1,15 @@
+let numero1=Number(prompt("Digite o primeiro número: "))
+let numero2=Number(prompt("Digite o segundo número: "))
+let numero3=Number(prompt("Digite o terceiro número: "))
+
+if (numero1>numero2 && numero1>numero3){
+  window.alert("O maior número é: "+numero1)
+}
+else if (numero2>numero1 && numero2>numero3){
+  window.alert("O maior número é: "+numero2)
+}
+else{
+  window.alert("O maior número é: "+numero3)
+}
+  
 

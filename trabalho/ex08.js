3335f883c7d7f7ae911d8a9a1c7ffd7eb45f8830
DimitriPorto03 <133895461+DimitriPorto03@users.noreply.github.com>
2024-03-31 14:51:12 -0300

    Update ex08.js

--- trabalho/ex08.js
+++ trabalho/ex08.js
@@ -1,1 +1,17 @@
+if (lado1==0 || lado2==0 || lado3==0){
+  window.alert("Não é um triângulo.")
+}
+else{
+  if (lado1==lado2 && lado2==lado3){
+    window.alert("O triângulo é equilátero.")
+  }
+  else if (lado1==lado2 || lado2==lado3 || lado1==lado3){
+    window.alert("O triângulo é isósceles.")
+  }
+  else{
+    window.alert("O triângulo é escaleno.")
+  }
+    
+}
+
 

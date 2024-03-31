95692c321790fecc92266b97096a7fea52c5561b
DimitriPorto03 <133895461+DimitriPorto03@users.noreply.github.com>
2024-03-31 14:38:08 -0300

    Update ex06.js

--- trabalho/ex06.js
+++ trabalho/ex06.js
@@ -1,1 +1,12 @@
+function somar(numero1,numero2,numero3){
+return numero1+numero2+numero3
+}
+
+let numero1=Number(prompt("Digite o primeiro número: "))
+let numero2=Number(prompt("Digite o segundo número: "))
+let numero3=Number(prompt("Digite o terceiro número: "))
+
+let resultado=somar(numero1,numero2,numero3)
+window.alert("A soma desses três números resulta em "+resultado)
+
 
